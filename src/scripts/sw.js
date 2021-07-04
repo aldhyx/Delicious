4b141c95cbb52bc13990ab169c7b024294d0bc2d
frialdhy ketty <aldyzkobf@gmail.com>
2021-07-04 21:31:02 +0800

    feat: adding cache

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,20 +1,27 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-restricted-globals */
 import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
+
+const { assets } = global.serviceWorkerOption;
 
 self.addEventListener('install', (event) => {
   console.log('Installing Service Worker ...');
 
-  // TODO: Caching App Shell Resource
+  // Caching App Shell Resource
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
 });
 
 self.addEventListener('activate', (event) => {
   console.log('Activating Service Worker ...');
 
-  // TODO: Delete old caches
+  // Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
   // console.log('from workers', event.request);
-  // TODO: Add/get fetch request to/from caches
+
+  // Add/get fetch request to/from caches
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });

ccad4edb06951444b28ff4f585e94a5d908dbd25
frialdhy ketty <aldyzkobf@gmail.com>
2021-07-11 23:57:57 +0800

    fix: change cache name to static

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   BASE_IMAGE_URL_SMALL: 'https://restaurant-api.dicoding.dev/images/small/',
   BASE_IMAGE_URL_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium/',
   BASE_IMAGE_URL_LARGE: 'https://restaurant-api.dicoding.dev/images/large/',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'delicious-v0.1.0',
   DATABASE_NAME: 'delicious-catalogue-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurant',

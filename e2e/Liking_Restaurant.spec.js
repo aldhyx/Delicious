d0e3da82713a3c58abc2cf1c6997ef70dc6baa6e
frialdhy ketty <aldyzkobf@gmail.com>
2021-07-23 21:15:27 +0800

    implement e2e

---
+++ e2e/Liking_Restaurant.spec.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+
+Feature('Liking Restaurant');
+
+Scenario('like unlike one restaurant', async ({ I }) => {
+  I.amOnPage('/');
+  I.seeElement('.post-item .post-item__title a');
+  const firstFilm = locate('.post-item .post-item__title a').first();
+  const firstFilmTitle = await I.grabTextFrom(firstFilm);
+  I.click(firstFilm);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+  I.amOnPage('/#/favorite');
+  I.seeElement('.post-item');
+
+  const likedFilmTitle = await I.grabTextFrom('.post-item .post-item__title a');
+  assert.strictEqual(firstFilmTitle, likedFilmTitle);
+
+  I.seeElement('.post-item .post-item__title a');
+  I.click(firstFilm);
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+  I.amOnPage('/#/favorite');
+  I.dontSeeElement('.post-item');
+});
